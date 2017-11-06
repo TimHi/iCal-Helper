473f5da2a378c62262007b27b9ecea9df3be6b14
TimHiller <Tim.Hiller@stud.h-da.de>
2017-11-06 22:20:18 +0100

    Working on gui

--- src/main/java/calender/Main.java
+++ src/main/java/calender/Main.java
@@ -3,17 +3,12 @@ package calender;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.stage.Stage;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,68 +23,50 @@ public class Main extends Application {
 
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("Main.fxml"));
+            loader.setLocation(getClass().getResource("/MainGUI.fxml"));
             Parent root = loader.load();
-            Scene scene = new Scene(root, 300, 275);
+            Scene scene = new Scene(root, 783, 434);
 
-            stage.setTitle("FXML Welcome");
+            stage.setTitle("iCal Helper");
+            scene.setOnDragOver(new EventHandler<DragEvent>() {
+                @Override
+                public void handle(DragEvent event) {
+                    Dragboard db = event.getDragboard();
+                    if (db.hasFiles()) {
+                        event.acceptTransferModes(TransferMode.COPY);
+                    } else {
+                        event.consume();
+                    }
+                }
+            });
+
+            // Dropping over surface
+            scene.setOnDragDropped(new EventHandler<DragEvent>() {
+                @Override
+                public void handle(DragEvent event) {
+                    Dragboard db = event.getDragboard();
+                    boolean success = false;
+                    if (db.hasFiles()) {
+                        success = true;
+                        String filePath = null;
+                        for (File file:db.getFiles()) {
+                            filePath = file.getAbsolutePath();
+                            System.out.println(filePath);
+                            iCal i = new iCal(file);
+                            i.readCSV();
+                        }
+                    }
+                    event.setDropCompleted(success);
+                    event.consume();
+                }
+            });
             stage.setScene(scene);
             stage.show();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        /*
-        TextFlow textFlow = new TextFlow();
-        Font font = new Font("Arial", 15);
-
-        Text text1 = new Text("Drag & Drop CSV File here");
-        text1.setFill(Color.BLACK);
-        text1.setFont(font);
-
-        textFlow.getChildren().addAll(text1);
-
-        Group group = new Group(textFlow);
-        Scene scene = new Scene(group, 650, 150, Color.WHITE);
-        stage.setTitle("iCal Helper");
-
-
-        scene.setOnDragOver(new EventHandler<DragEvent>() {
-            @Override
-            public void handle(DragEvent event) {
-                Dragboard db = event.getDragboard();
-                if (db.hasFiles()) {
-                    event.acceptTransferModes(TransferMode.COPY);
-                } else {
-                    event.consume();
-                }
-            }
-        });
-
-        // Dropping over surface
-        scene.setOnDragDropped(new EventHandler<DragEvent>() {
-            @Override
-            public void handle(DragEvent event) {
-                Dragboard db = event.getDragboard();
-                boolean success = false;
-                if (db.hasFiles()) {
-                    success = true;
-                    String filePath = null;
-                    for (File file:db.getFiles()) {
-                        filePath = file.getAbsolutePath();
-                        System.out.println(filePath);
-                        iCal i = new iCal(file);
-                        i.readCSV();
-                    }
-                }
-                event.setDropCompleted(success);
-                event.consume();
-            }
-        });
-
-        stage.setScene(scene);
-        stage.show();
-    */
     }
 
 }
