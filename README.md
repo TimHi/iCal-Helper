458ca3e5400721dd0337fd990a5144fcef76848c
TimHiller <Tim.Hiller@stud.h-da.de>
2017-11-07 00:31:32 +0100

    Table View

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Convert CSV Files to ical Files ready to import into any calender of your choice
 
 ### CSV  
   
-  
+  In the resources folder is a formatted example csv.   
+  In the current state Dates have to be in the format "dd.mm.yy mm:HH"
--- src/main/java/calender/Main.java
+++ src/main/java/calender/Main.java
@@ -1,14 +1,23 @@
 package calender;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.cell.ComboBoxListCell;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,7 +26,11 @@ public class Main extends Application {
     public static void main(String[] args) {
         Application.launch(args);
     }
-
+    ListView<String> list = new ListView<String>();
+    ObservableList<String> data = FXCollections.observableArrayList(
+            "chocolate", "salmon", "gold", "coral", "darkorchid",
+            "darkgoldenrod", "lightsalmon", "black", "rosybrown", "blue",
+            "blueviolet", "brown");
     @Override
     public void start(Stage stage) throws Exception {
 
@@ -26,8 +39,9 @@ public class Main extends Application {
             loader.setLocation(getClass().getResource("/MainGUI.fxml"));
             Parent root = loader.load();
             Scene scene = new Scene(root, 783, 434);
-
             stage.setTitle("iCal Helper");
+
+
             scene.setOnDragOver(new EventHandler<DragEvent>() {
                 @Override
                 public void handle(DragEvent event) {
@@ -60,6 +74,10 @@ public class Main extends Application {
                     event.consume();
                 }
             });
+            ObservableList<String> names = FXCollections.observableArrayList(
+                    "Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise");
+            ListView<String> tableView = new ListView<String>(names);
+
             stage.setScene(scene);
             stage.show();
 
